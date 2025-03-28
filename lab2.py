fcadeb662bb6a556f76f5e3b0f990db2c7379fe0
Andrey Baryshev <andrey.bar66@gmail.com>
2025-03-29 00:14:03 +0500

    Update lab2

--- lab2.py
+++ lab2.py
@@ -1,19 +1,23 @@
+def dfs(matrix: list, visited: list, vertex: int):
+    if visited[vertex]:
+        return "N", visited
+    elif len(visited) >= vertex:
+        return ("A",)
 
-def BFS(matrix: list[int]):
-    n = len(matrix)
+    visited[vertex] = True
 
-
-    visited = 0
-
-
-    return visited == n
-
+    for i, is_connected in enumerate(matrix[vertex]):
+        if is_connected:
+            return dfs(matrix, visited, i)
 
 def main():
     count = int(input("Enter counter: "))
-    matrix = [x for x in range(count)]
-    BFS(matrix)
-
+    matrix = [list(map(int, input().split())) for _ in range(count)]
+    visited = [False] * len(matrix)
+    result = dfs(matrix, visited, 0)
+    print(*result[0])
+    if result[0] == "N":
+        print(*result[1])
 
 if __name__ == '__main__':
     main()
