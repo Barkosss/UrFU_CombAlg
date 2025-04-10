0584ca0bb62f850d8d9e4e1e6f8ff699ff335097
Barkosss <andrey.bar66@gmail.com>
2025-04-10 22:43:06 +0500

    Update lab3

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -1,3 +1,30 @@
-//
-// Created by barkos on 10.04.2025.
-//
+// Lab3: https://contest.yandex.ru/contest/46568/problems/C/
+/*
+ * Найти v-w путь в сети с неотрицательными весами,
+ * решающий задачу о пути наибольшего веса,
+ * где вес пути равен сумме весов дуг.
+ *
+ * Метод решения: алгоритм построения пути в бесконтурной сети.
+ *
+ * Число -32768 означает, что данная дуга отсутствует.
+ */
+
+#include<iostream>
+#include<fstream>
+
+using std::cout;
+using std::endl;
+using std::ofstream;
+using std::ifstream;
+
+int main() {
+    ifstream inFile("in.txt");
+    ofstream outFile("out.txt");
+
+    if (!inFile.is_open() || inFile.eof()) {
+        cout << "File is not found or is empty!" << endl;
+        return 404;
+    }
+
+    // ...
+}
